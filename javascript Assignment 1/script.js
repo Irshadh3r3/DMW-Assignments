3da1ca2048da0a8f8220e0c379497dca25246a02
Irshadh3r3 <156067289+Irshadh3r3@users.noreply.github.com>
2024-03-06 20:20:21 +0500

    Add files via upload

---
+++ javascript Assignment 1/script.js
@@ -0,0 +1,6 @@
+let firstname = prompt("Please enter your first name:");
+let lastname = prompt("Please enter your last");
+console.log(firstname + " " +lastname);
+alert("Hello, "+ firstname +" "+ lastname+". Welcome to website!");
+
+
