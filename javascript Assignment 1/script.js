2ff6dc304db259558421a1e58692c25d27ce849e
Irshadh3r3 <156067289+Irshadh3r3@users.noreply.github.com>
2024-03-07 11:05:57 +0500

    script.js

--- javascript Assignment 1/script.js
+++ javascript Assignment 1/script.js
@@ -1,6 +1,7 @@
 let firstname = prompt("Please enter your first name:");
+let middlename = prompt("Please enter your middle name");
 let lastname = prompt("Please enter your last");
-console.log(firstname + " " +lastname);
-alert("Hello, "+ firstname +" "+ lastname+". Welcome to website!");
+console.log(firstname +" "+ middlename +" "+lastname);
+alert("Hello, "+ firstname +" "+ middlename + " " + lastname+". Welcome to website!");
 
 
