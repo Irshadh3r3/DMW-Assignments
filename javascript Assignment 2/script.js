f51d34b65b962cce1cfe792b09e0912c4bc0c493
Irshadh3r3 <156067289+Irshadh3r3@users.noreply.github.com>
2024-03-12 15:40:27 +0000

    Add files via upload

---
+++ javascript Assignment 2/script.js
@@ -0,0 +1,5 @@
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
+let num3 = prompt("Enter the third number:");
+let sum = Number(num1) + Number(num2) + Number(num3);
+console.log("The sum of the three numbers is: " + sum); 
